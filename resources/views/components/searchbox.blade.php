833507b80515fdb7abfd39fa23d4cc343bff0af7
Alexander Vasilev <=>
2024-04-23 19:27:44 +0200

    wip

--- resources/views/components/searchbox.blade.php
+++ resources/views/components/searchbox.blade.php
@@ -5,16 +5,49 @@
 
         <form method="GET" action="{{ route('search') }}" class="mx-auto">
             <div class="flex items-center mx-auto">
-                <label for="search-dropdown" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Искать</label>
-                <select
-                    id="searchArea"
-                    class="px-6 flex-shrink-0 z-10 inline-flex items-center py-3 x-4 text-sm font-medium text-center text-gray-900 bg-gray-100 border border-gray-300 rounded-s-lg hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700 dark:text-white dark:border-gray-600"
-                    name="area">
-                    <option value="book" @if(request()->get('area') === 'book') selected @endif>Книги</option>
-                    <option value="author" @if(request()->get('area') === 'author') selected @endif>Авторы</option>
-                    <option value="series" @if(request()->get('area') === 'series') selected @endif>Серии</option>
-                    <option value="genre" @if(request()->get('area') === 'genre') selected @endif>Жанры</option>
-                </select>
+                <div class="relative">
+                    <label for="search-dropdown" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Искать</label>
+                    <select
+                        id="searchArea"
+                        class="appearance-none px-6 flex-shrink-0 z-10 inline-flex items-center py-3 x-4 text-sm font-medium text-center text-gray-900 bg-gray-100 border border-gray-300 rounded-s-lg hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700 dark:text-white dark:border-gray-600"
+                        name="area">
+                        <option value="book" @if(request()->get('area') === 'book') selected @endif>Книги
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                                 stroke="currentColor" class="w-6 h-6">
+                                <path stroke-linecap="round" stroke-linejoin="round"
+                                      d="M8.25 15 12 18.75 15.75 15m-7.5-6L12 5.25 15.75 9"/>
+                            </svg>
+                        </option>
+                        <option value="author" @if(request()->get('area') === 'author') selected @endif>Авторы</option>
+                        <option value="series" @if(request()->get('area') === 'series') selected @endif>Серии</option>
+                        <option value="genre" @if(request()->get('area') === 'genre') selected @endif>Жанры</option>
+                    </select>
+
+                    <div
+                        class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700 dark:text-gray-100">
+                        <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                            <path
+                                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"/>
+                        </svg>
+                    </div>
+                </div>
+
+
+{{--                <div class="relative">--}}
+{{--                    <select id="searchArea"--}}
+{{--                            class="appearance-none px-6 py-3 w-full text-sm font-medium text-gray-900 bg-gray-100 border border-gray-300 rounded-lg hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700 dark:text-white dark:border-gray-600">--}}
+{{--                        <option value="book">Книги</option>--}}
+{{--                        <!-- Другие элементы -->--}}
+{{--                    </select>--}}
+{{--                    <div--}}
+{{--                        class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700 dark:text-gray-100">--}}
+{{--                        <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">--}}
+{{--                            <path--}}
+{{--                                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"/>--}}
+{{--                        </svg>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+
 
                 <div class="relative w-full">
                     <input value="{{ request('search') }}" type="search" id="search-dropdown"
